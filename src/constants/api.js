ac4417c6c11c1d235e82fe3e984fd2f37d07b81f
刘阳 <numbksco@gmail.com>
2017-10-13 00:57:04 +0800

    切换新的 CORS Server

--- src/constants/api.js
+++ src/constants/api.js
@@ -6,4 +6,4 @@ import {
 export const ISSUES_BASE_URL = `https://api.github.com/repos/${USERNAME}/${REPONAME}/issues`;
 
 export const AUTHORIZE_URL = 'http://github.com/login/oauth/authorize';
-export const ACCESS_URL = 'https://github-access-token-rljyinvnwq.now.sh/';
+export const ACCESS_URL = 'https://github-access-token-pdhnmrdjvs.now.sh/';
